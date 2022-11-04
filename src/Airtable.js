8ae81fb63f0ed3ab38f0a8d7e06812d4c4964930
Ernie Park <ernestipark@gmail.com>
2022-11-04 15:27:53 -0400

    make dynamic with logo

--- src/Airtable.js
+++ src/Airtable.js
@@ -1,7 +1,10 @@
 import {
   Container,
+  Box,
 } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import imgHubSpot from './images/hubspot.png';
+import imgStripe from './images/stripe.png';
 
 
 const companyData = {
@@ -9,11 +12,13 @@ const companyData = {
     airtableLink: "https://airtable.com/embed/shrHi8a6NsiPXQv00?backgroundColor=blue&viewControls=on",
     title: 'HubSpot',
     url: 'https://hubspot.com',
+    logoRef: imgHubSpot,
   },
   'stripe': {
     airtableLink: "https://airtable.com/embed/shrHi8a6NsiPXQv00?backgroundColor=blue&viewControls=on",
     title: 'Stripe',
     url: 'https://stripe.com',
+    logoRef: imgStripe,
   },
 };
 
@@ -21,10 +26,18 @@ const Airtable = () => {
 
   const [company, setCompany] = useState('hubspot');
 
-  const { airtableLink, title, url } = companyData[company];
+  const { logoUrl, airtableLink, title, url, logoRef } = companyData[company];
 
   return (
     <Container>
+      <Box>
+        Switcher
+      </Box>
+      <Box>
+        <a target="_blank" href={url}>
+          <img src={logoRef} width="300px"/>
+        </a>
+      </Box>
       <iframe className="airtable-embed" src={airtableLink} frameborder="0" onmousewheel="" width="100%" height="800" style={{background: 'transparent', border: '1px solid #ccc'}}></iframe>
 
     </Container>
